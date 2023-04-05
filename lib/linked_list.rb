97b411c09fdc876058097f239a4f74210bbea3d9
Adam DePaolo <amdepaolo@gmail.com>
2023-04-05 16:12:48 -0400

    commit tree 59501e7caa15f34a8316d273cb41486d70680579

--- lib/linked_list.rb
+++ lib/linked_list.rb
@@ -8,7 +8,30 @@ class LinkedList
   end
 
   def nth_from_end(n)
-    # your code here
+    if head.nil?|| n < 1
+      return nil
+    end
+
+    current_node = head
+    list_length = 0
+
+    while current_node
+      current_node = current_node.next_node
+      list_length +=1
+    end
+
+    node_position = list_length - n
+    desired_node = head
+
+    if node_position < 0
+      return nil
+    end
+
+    node_position.times do 
+      desired_node = desired_node.next_node
+    end
+
+    return desired_node.value
   end
 
 end
